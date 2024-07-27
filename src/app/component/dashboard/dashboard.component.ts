b4820a6335e545bdf8b74102257c5f5d7b45f0c7
Luiza <d2021007820@unifei.edu.br>
2024-07-27 11:07:23 -0300

    dashboard criado

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  constructor (private route: Router) {}
+  ngOnInit(): void {
+    
+  }
 
+  dirperfil(){
+    this.route.navigate(['/pagina-inicial']);
+  }
+  dirprodutos(){
+    this.route.navigate(['/pagina-inicial']);
+  }
+  direstoque(){
+    this.route.navigate(['/pagina-inicial']);
+  }
+  dirregistro(){
+    this.route.navigate(['/pagina-inicial']);
+  }
+  dirusuarios(){
+    this.route.navigate(['/pagina-inicial']);
+  }
 }
